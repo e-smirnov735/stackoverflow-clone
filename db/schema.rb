4e1124977588d8e916b7b4207c6cc4c33a825cd0
Evgeny Smirnov <e-smirnov@outlook.com>
2023-09-18 16:47:17 +0300

    feat: add answer model & tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_18_125540) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_18_133745) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.text "body", null: false
+    t.bigint "question_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "title", null: false
@@ -21,4 +29,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_18_125540) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "answers", "questions"
 end
