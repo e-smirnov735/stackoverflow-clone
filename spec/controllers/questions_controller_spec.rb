3cd53428ee435ef7cd406466da4777d1a63e39e1
Evgeny Smirnov <e-smirnov@outlook.com>
2023-09-23 20:57:03 +0300

    feat: add tests to #new

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -27,4 +27,26 @@ RSpec.describe QuestionsController, type: :controller do
       expect(response).to render_template :show
     end
   end
+
+  describe 'GET #new' do
+    before { get :new }
+
+    it 'assigns a new Question to @question' do
+      expect(assigns(:question)).to be_a_new(Question)
+    end
+
+    it 'render new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  # describe 'POST #create' do
+  #   context 'with valid attributes' do
+  #     it 'saves a new question in the database' do
+  #     end
+
+  #     it 'with invalid attributes' do
+  #     end
+  #   end
+  # end
 end
