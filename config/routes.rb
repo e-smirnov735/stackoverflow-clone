f1b4ac2c58162fa49029a9b5fe80ffde69694b2f
Evgeny Smirnov <e-smirnov@outlook.com>
2023-10-02 22:27:06 +0300

    add tests: only auth users can created quest&answ

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :questions do
-    resources :answers, shallow: true
+    resources :answers, except: :index, shallow: true do
+      post :inline_create, on: :collection
+    end
   end
 
   root to: 'questions#index'
