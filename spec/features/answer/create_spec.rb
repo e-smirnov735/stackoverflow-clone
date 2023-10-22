6e289c4d31e20519f3eec19fa36582bee9a3ffbb
Evgeny Smirnov <e-smirnov@outlook.com>
2023-10-22 12:33:48 +0300

    feat: create answer and question with files

--- spec/features/answer/create_spec.rb
+++ spec/features/answer/create_spec.rb
@@ -23,6 +23,16 @@ describe 'User can create answer for question', "
       end
     end
 
+    it 'asks a answer with attached file', js: true do
+      fill_in 'Body', with: 'text text text'
+
+      attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+      click_on 'Post Your Answer'
+
+      expect(page).to have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
+    end
+
     it 'create answer with errors', js: true do
       click_on 'Post Your Answer'
 
