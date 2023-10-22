01b4a3c501838551ba2b88ff288bd61529f9e344
Evgeny Smirnov <e-smirnov@outlook.com>
2023-10-22 16:59:46 +0300

    feat: author add files when edit answer & question

--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -19,11 +19,10 @@ I'd like to be able to edit my answer
     before do
       sign_in(user)
       visit question_path(question)
+      click_on 'Edit'
     end
 
     it 'edits his answer', js: true do
-      click_on 'Edit'
-
       within '.answers' do
         fill_in 'Your answer', with: 'edited answer'
         click_on 'Save'
@@ -35,14 +34,25 @@ I'd like to be able to edit my answer
     end
 
     it 'edits his answer with errors', js: true do
-      click_on 'Edit'
-
       within '.answers' do
         fill_in 'Your answer', with: ''
         click_on 'Save'
 
         expect(page).to have_selector 'textarea'
         expect(page).to have_content "Body can't be blank"
+      end
+    end
+
+    it 'edits his answer with add files', js: true do
+      within '.answers' do
+        fill_in 'Body', with: 'edit body'
+
+        attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+
+        click_on 'Save'
+
+        expect(page).to have_link 'rails_helper.rb'
+        expect(page).to have_link 'spec_helper.rb'
       end
     end
   end
